638614e6c17b71ccfeb6c6b5a5b91b1d1b46ed32
T. Thiery <thomas.thiery+git@gmail.com>
2021-11-20 14:56:37 +0100

    Add Boolean Combinations
    #44 non-breaking

--- src/Violet.WorkItems.Abstractions/WiqlModel.cs
+++ src/Violet.WorkItems.Abstractions/WiqlModel.cs
@@ -1,13 +1,25 @@
-namespace Violet.WorkItems.Query;
+using System.Collections.Immutable;
 
+namespace Violet.WorkItems.Query;
 
 public record QueryClause();
 
+public record BooleanMultiClause(ImmutableArray<QueryClause> SubClauses)
+    : QueryClause();
 public record ProjectClause(string ProjectCode)
+    : QueryClause();
+public record WorkItemIdClause(string WorkItemId)
     : QueryClause();
 public record WorkItemTypeClause(string WorkItemType)
     : QueryClause();
 public record PropertyClause(string PropertyName)
+    : QueryClause();
+
+public record AndClause(ImmutableArray<QueryClause> SubClauses)
+    : BooleanMultiClause(SubClauses);
+public record OrClause(ImmutableArray<QueryClause> SubClauses)
+    : BooleanMultiClause(SubClauses);
+public record NotClause(QueryClause SubClause)
     : QueryClause();
 
 public record StringMatchClause(string PropertyName, string Match)
@@ -17,12 +29,17 @@ public record ValueMatchClause(string PropertyName, string[] Values, bool Not)
 public record JoinClause(string PropertyName, QueryClause[] Clauses, bool ReturnData)
     : PropertyClause(PropertyName);
 
-public record WorkItemsQuery(params QueryClause[] Clauses);
-
-public static class WorkItemsQueryExtensions
+public record WorkItemsQuery(ImmutableArray<QueryClause> Clauses)
 {
-    public WorkItemsQuery OfProjectCodeAndType(string projectCode, string workItemType)
-        => new WorkItemQuery(
+    public WorkItemsQuery(params QueryClause[] Clauses)
+        : this(Clauses.ToImmutableArray())
+    { }
+}
+
+public static class CommonQueries
+{
+    public static WorkItemsQuery OfProjectCodeAndType(string projectCode, string workItemType)
+        => new WorkItemsQuery(
             new ProjectClause(projectCode),
             new WorkItemTypeClause(workItemType)
         );
