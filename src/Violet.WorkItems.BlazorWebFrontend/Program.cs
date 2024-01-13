763deb839177df407d3d0186661ade6a43db7d2a
T. Thiery <thomas.thiery+git@gmail.com>
2024-01-13 21:59:44 +0100

    Change UI Toolkit to Fluent UI
    #50

--- src/Violet.WorkItems.BlazorWebFrontend/Program.cs
+++ src/Violet.WorkItems.BlazorWebFrontend/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MudBlazor.Services;
+using Microsoft.FluentUI.AspNetCore.Components;
 using Violet.WorkItems.BlazorWebFrontend;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -10,6 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<WorkItemService>();
 builder.Services.AddScoped<WorkItemDescriptorService>();
-builder.Services.AddMudServices();
+builder.Services.AddFluentUIComponents();
 
 await builder.Build().RunAsync();
